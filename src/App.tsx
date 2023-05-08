abdc0d3022ec9f3915519d0d2afc12d6925a6c80
onedrako <onedrako@gmail.com>
2023-05-08 15:13:42 -0600

    Clase 1. Integración con React - Creación e integración con Bulma CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "bulma/css/bulma.css"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+  <div className='App'>
+    <button className='button is-primary'>Hola Mundo</button>
+  </div>
+  )
 }
 
 export default App;
