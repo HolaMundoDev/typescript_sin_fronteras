07ad776eddbdc94a99e6d7d2ae873f3bcd63c5c3
onedrako <onedrako@gmail.com>
2023-05-08 16:29:51 -0600

    Clase 5. useState

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,11 +3,13 @@ import { ChangeEventHandler } from 'react';
 interface InputProps {
   placeholder: string;
   handleChange: ChangeEventHandler<HTMLInputElement>
+  name: string;
 }
 
-export default function Input({ placeholder, handleChange }: InputProps) {
+export default function Input({ placeholder, handleChange, name }: InputProps) {
   return (
     <input
+      name={name}
       placeholder={placeholder}
       onChange={handleChange}
     />
--- src/forms/User.tsx
+++ src/forms/User.tsx
@@ -1,21 +1,34 @@
-import { ChangeEventHandler, MouseEventHandler } from "react";
+import { ChangeEventHandler, MouseEventHandler, useState } from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 
+type UserFormState = {
+  name: string;
+  lastName: string;
+}
+
+const Initialvalue: UserFormState = {
+  name: '',
+  lastName: ''
+}
+
 export default function UserForm(){
+  const [form, setForm] = useState(Initialvalue)
+
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    console.log(e.target.value);
+    const name = e.target.name as keyof UserFormState;
+    setForm({...form, [name]: e.target.value})
   }
 
   const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
     e.preventDefault();
-    console.log('Hola Mundo');
+    console.log(form)
   }
 
   return (
     <>
-      <Input placeholder="Nombre" handleChange={handleChange}/>
-      <Input placeholder="Apellido" handleChange={handleChange}/>
+      <Input name="name" placeholder="Nombre" handleChange={handleChange}/>
+      <Input name="lastname" placeholder="Apellido" handleChange={handleChange}/>
       <Button handleClick={handleClick}>Hola Mundo</Button>
     </>
   )
