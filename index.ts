706dc4d244385b58de98a39248482c65b24bf317
onedrako <onedrako@gmail.com>
2023-05-11 15:40:25 -0600

    Clase 2. Decoradores de clase

--- index.ts
+++ index.ts
@@ -1,6 +1,16 @@
+function Route(ruta: string) {
+  return (constructor: Function) => {
+    console.log("Ejecutando el decorador de ruta")
+    constructor.prototype.route = ruta
+  }
+}
+
 @Route("/productos")
 class Productos {
   find() {
     return "Producto"
   }
 }
+
+const p = new Productos() as any
+console.log(p.route)
