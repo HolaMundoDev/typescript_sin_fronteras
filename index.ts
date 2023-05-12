b77174b5f5988f550c632808094a6c09c228787b
onedrako <onedrako@gmail.com>
2023-05-11 18:03:39 -0600

    Clase 3. Decorador de métodos

--- index.ts
+++ index.ts
@@ -5,12 +5,26 @@ function Route(ruta: string) {
   }
 }
 
-@Route("/productos")
-class Productos {
-  find() {
-    return "Producto"
+// function Method(target: any, methodName: string, descriptor: PropertyDescriptor) {
+function Method(method: string) {
+  return (target: any, methodName: string, descriptor: PropertyDescriptor) => {
+    console.log(methodName, descriptor)
+    const original = descriptor.value
+    descriptor.value = function (...args: any) {
+      console.log("Antes del método decorado")
+      original.call(this, ...args) // Ejecutamos el método de la definición original
+      console.log("Después de ejecutar el método decorado")
+    }
   }
 }
 
-const p = new Productos() as any
-console.log(p.route)
+@Route("/productos")
+class Productos {
+  @Method("GET")
+  find(val: string) {
+    console.log("Soy el método find " + val)
+  }
+}
+
+const p = new Productos()
+p.find("hola mundo")
